4d52c7c9f76165b6bbbdf895e41f17fc73ea0cdf
SeanQQQ <sean.savanah1999@gmail.com>
2023-08-10 22:22:28 +1000

    Added Basic Graphing

---
+++ src/NodeGraph.js
@@ -0,0 +1,91 @@
+import * as d3 from "d3";
+import React, { useEffect, useRef, useState } from 'react';
+import { select } from 'd3-selection';
+
+export default function NodeGraph({
+  data,
+  width = 1600,
+  height = 960
+}) {
+
+  const ref = useRef();
+
+  useEffect(()=> {
+    const svg = d3.select(ref.current);
+    let rectHolders = svg.selectAll("g").data(data.nodes).join("g")
+    .attr("transform", d => ("translate(" + (data.nodes.indexOf(d)*(width/12+10)) + "," + ( 70) + ")"))
+    .on('click', (e, d) => console.log(d.name))
+    .attr('data-id', d => d.id)
+    ;
+
+    rectHolders.append("rect")
+      .attr("stroke", "blue")
+      .attr("width", width/12)
+      .attr("height", width/24)
+      .attr('data-id', d => d.id)
+      .attr("fill-opacity", "0")
+    
+    
+    rectHolders.append("text")
+      .text( d => d.name)
+      .style("font-size", "11px")
+      .attr('x', width/12 /2)
+      .attr('y', width/24 / 2)
+      .attr('text-anchor', "middle")      
+      .attr("font-weight", "bold");
+
+    ;
+    
+    let rectangles = svg.selectAll("rect")
+    // .data(data.nodes).join("rect")
+    // .attr("x", d => data.nodes.indexOf(d)*60 + 10)
+    // .attr("y",  d => data.nodes.indexOf(d)+ 70)
+    // .attr("width", 40)
+    // .attr("height", 20)
+    // .attr('data-id', d => d.id)
+    // .attr("fill", "none")
+    // .attr("stroke", "red");
+
+    svg.selectAll(".link")
+      .data(data.links)
+      .join("path")
+      .attr("class", "link")
+      .attr("d", d => {
+        
+        let sourcegroup = rectHolders.nodes().find(c => c.__data__.id === d.source)
+        let targetgoup = rectHolders.nodes().find(c => c.__data__.id === d.target)
+        let sourceRect = rectangles.nodes().find( c => c.__data__.id === d.source);
+        let targetRect = rectangles.nodes().find( c => c.__data__.id === d.target);
+        
+        let sourcex = parseFloat(sourcegroup.getAttribute("transform").match(/translate\(([^,]+),/)[1]) + parseFloat(sourceRect.getAttribute('width'))/2;
+        let sourcey = parseFloat(sourcegroup.getAttribute("transform").match(/translate\([^,]+,\s*([^)]+)\)/)[1]) + parseFloat(sourceRect.getAttribute('height'));
+
+        let targetx = parseFloat(targetgoup.getAttribute("transform").match(/translate\(([^,]+),/)[1]) + parseFloat(targetRect.getAttribute('width'))/2;
+        let targety = parseFloat(targetgoup.getAttribute("transform").match(/translate\([^,]+,\s*([^)]+)\)/)[1]) + parseFloat(targetRect.getAttribute('height'));
+        
+        console.log(`SX ${sourcex} SY ${sourcey}`)
+
+        const dx = targetx - sourcex;
+        const dy = targety - sourcey;
+        const dr = Math.sqrt(dx * dx + dy * dy);
+
+        return `M${sourcex},${sourcey}A${1},${.2} 0,0,0 ${targetx},${targety}`;
+      })
+      .attr("fill", "none")
+      .attr("stroke", "red")
+  }
+  )
+
+  return (<svg 
+    ref={ref}
+    width={width}
+    height={height}
+   />)
+}
+
+//data format:
+//data = { nodes:[], links:[] }
+
+// node = {id, name, creditPointsm}
+
+// link = {source, target}
---
+++ src/graph.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './index.css';
+import NodeGraph from './NodeGraph';
+
+
+function Graph() {
+
+  let data = {
+     nodes : [
+      {id : 1, name : 'Programming Basics'},
+      {id : 2, name : 'Software Production'},
+      {id : 3, name : 'Design Fundamentals'},
+      {id : 4, name : 'Database Design'},
+      {id : 5, name : 'Frontend Design'},
+      {id : 6, name : 'Backend Programming'},
+      {id : 7, name : 'Software Testing'},
+      {id : 8, name : 'Programming'},
+      {id : 9, name : 'Advanced Programming'},
+      {id : 10, name : 'Networking'},
+    ],
+    links : [
+      {source: 1, target: 8},
+      {source: 4, target: 6},
+      {source: 8, target: 9},
+      {source: 3, target: 5},
+      {source: 6, target: 10},
+      {source: 4, target: 10},
+    ] 
+    };
+
+  return (
+    <div className="App">
+        <NodeGraph data={data}/>
+    </div>
+  );
+}
+
+export default Graph;

6d1f9880615c4a72e7b9cf0a6f035dc3630814a2
SeanQQQ <sean.savanah1999@gmail.com>
2023-08-15 11:25:43 +1000

    Connected to API

--- src/NodeGraph.js
+++ src/NodeGraph.js
@@ -4,74 +4,73 @@ import { select } from 'd3-selection';
 
 export default function NodeGraph({
   data,
-  width = 1600,
-  height = 800
+  width = 960,
+  height = 960
 }) {
 
+  // 2  | 1200  600 , 960 480
+  // 3  | 1200  400 , 960 320
+  // 4  | 1200  300 , 960 240
+  // 6  | 1200  200 , 960 160
+  // 10 | 1200  120 , 960 96
+  // 12 | 1200  100 , 960 80
+  // 16 | 1200  75  , 960 60
+  // 20 | 1200  60  , 960 48
+  // 24 | 1200  50  , 960 40
+  // 48 | 1200  25  , 960 20
+
   const ref = useRef();
+
+  const margin = width/48;
+  const rectWidth = width/6;
+  const rectHeight = width/12;
 
   useEffect(()=> {
     const svg = d3.select(ref.current);
     let rectHolders = svg.selectAll("g").data(data.nodes).join("g")
-    .attr("transform", d => ("translate(" + (data.nodes.indexOf(d)*(width/4))%width + "," + (20+(Math.ceil((data.nodes.indexOf(d)+1)/4)-1)*(height/10)) + ")"))
+    .attr("transform", d => ("translate(" + (data.nodes.indexOf(d)*(width/4) + margin)%width + "," + ((Math.floor(data.nodes.indexOf(d)/4))*(height/10) + margin) + ")"))
     .on('click', (e, d) => console.log(d.name))
-    .attr('data-id', d => d.id);
+    .attr('data-id', d => d.subjectId);
 
-    rectHolders.append("rect")
+    let rectangles = rectHolders.append("rect")
       .attr("stroke", "blue")
-      .attr("width", width/12)
-      .attr("height", width/24)
-      .attr('data-id', d => d.id)
+      .attr("width", rectWidth)
+      .attr("height", rectHeight)
+      .attr('data-id', d => d.subjectId)
       .attr("fill-opacity", "0")
     
-    
     rectHolders.append("text")
-      .text( d => d.name)
+      .text( d => d.subjectName)
       .style("font-size", "11px")
-      .attr('x', width/12 /2)
-      .attr('y', width/24 / 2)
+      .attr('x', rectWidth/2)
+      .attr('y', rectHeight/2)
       .attr('text-anchor', "middle")      
       .attr("font-weight", "bold");
-
-    ;
     
-    let rectangles = svg.selectAll("rect")
-    // .data(data.nodes).join("rect")
-    // .attr("x", d => data.nodes.indexOf(d)*60 + 10)
-    // .attr("y",  d => data.nodes.indexOf(d)+ 70)
-    // .attr("width", 40)
-    // .attr("height", 20)
-    // .attr('data-id', d => d.id)
-    // .attr("fill", "none")
-    // .attr("stroke", "red");
-
     svg.selectAll("path")
     .append("path")
       .data(data.links)
       .join("path")
       .attr("class", "link")
       .attr("d", d => {
-        let sourcegroup = rectHolders.nodes().find(c => c.__data__.id === d.source)
-        let targetgoup = rectHolders.nodes().find(c => c.__data__.id === d.target)
-        let sourceRect = rectangles.nodes().find( c => c.__data__.id === d.source);
-        let targetRect = rectangles.nodes().find( c => c.__data__.id === d.target);
+        let sourcegroup = rectHolders.nodes().find(c => c.__data__.subjectId === d.source)
+        let targetgoup = rectHolders.nodes().find(c => c.__data__.subjectId === d.target)
+        let sourceRect = rectangles.nodes().find( c => c.__data__.subjectId === d.source);
+        let targetRect = rectangles.nodes().find( c => c.__data__.subjectId === d.target);
         
-        let sourcex = parseFloat(sourcegroup.getAttribute("transform").match(/translate\(([^,]+),/)[1]) + parseFloat(sourceRect.getAttribute('width'))/2;
-        let sourcey = parseFloat(sourcegroup.getAttribute("transform").match(/translate\([^,]+,\s*([^)]+)\)/)[1]) + parseFloat(sourceRect.getAttribute('height')/2);
-
-        let targetx = parseFloat(targetgoup.getAttribute("transform").match(/translate\(([^,]+),/)[1]) + parseFloat(targetRect.getAttribute('width'))/2;
-        let targety = parseFloat(targetgoup.getAttribute("transform").match(/translate\([^,]+,\s*([^)]+)\)/)[1]);        
-
-        const dx = targetx - sourcex;
-        const dy = targety - sourcey;
-        const dr = Math.sqrt(dx * dx + dy * dy);
-
-        return `M${sourcex},${sourcey} ${targetx},${targety}`; //`M${sourcex},${sourcey}A${1},${.2} 0,0,0 ${targetx},${targety}`; for arcs 
+        if(sourcegroup){
+          let sourcex = parseFloat(sourcegroup.getAttribute("transform").match(/translate\(([^,]+),/)[1]) + parseFloat(sourceRect.getAttribute('width'))/2;
+          let sourcey = parseFloat(sourcegroup.getAttribute("transform").match(/translate\([^,]+,\s*([^)]+)\)/)[1]) + parseFloat(sourceRect.getAttribute('height')/2);
+  
+          let targetx = parseFloat(targetgoup.getAttribute("transform").match(/translate\(([^,]+),/)[1]) + parseFloat(targetRect.getAttribute('width'))/2;
+          let targety = parseFloat(targetgoup.getAttribute("transform").match(/translate\([^,]+,\s*([^)]+)\)/)[1]);
+        
+          return `M${sourcex},${sourcey} ${targetx+20},${targety}`;
+        }
       })
-      .attr("fill", "none")
-      .attr("stroke", "red")
-      .attr("marker-end", "url(#head)")
-  }
+      .style("fill", "black")
+      .style("stroke", "red")
+      }, []
   )
 
   return (<svg 
--- src/graph.js
+++ src/graph.js
@@ -1,37 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.css';
 import NodeGraph from './NodeGraph';
 
 
 function Graph() {
+  const [data, setData] = useState(null);
 
-  let data = {
-     nodes : [
-      {id : 1, name : 'Programming Basics'},
-      {id : 2, name : 'Software Production'},
-      {id : 3, name : 'Design Fundamentals'},
-      {id : 4, name : 'Database Design'},
-      {id : 5, name : 'Frontend Design'},
-      {id : 6, name : 'Backend Programming'},
-      {id : 7, name : 'Software Testing'},
-      {id : 8, name : 'Programming'},
-      {id : 9, name : 'Advanced Programming'},
-      {id : 10, name : 'Networking'},
-    ],
-    links : [
-      {source: 1, target: 5},
-      {source: 4, target: 6},
-      {source: 8, target: 10},
-      {source: 5, target: 6},
-      {source: 6, target: 10},
-    ] 
-    };
 
-  return (
-    <div className="App">
-        <NodeGraph data={data}/>
-    </div>
-  );
+  useEffect(() =>{
+    fetch('https://localhost:7099/Subject').then((res) => res.json())
+    .then((d) => {
+        setData( {nodes: d.subjects, links: d.links} );
+    })
+  }, [])
+
+  if(data){
+    console.log(data);
+    return (
+      <div className="App">
+          <NodeGraph data={data}/>
+      </div>
+    );
+  }else{
+    return <div></div>
+  }
+
 }
 
 export default Graph;
